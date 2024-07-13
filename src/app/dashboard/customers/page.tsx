e064a15a89e34935ba94ffa54122d0b2e05d887d
shivampython <shivammishra.python@gmail.com>
2024-07-14 00:36:02 +0530

    learning to redirect server side components to another url using redirect

--- src/app/dashboard/customers/page.tsx
+++ src/app/dashboard/customers/page.tsx
@@ -1,4 +1,9 @@
+
+import { redirect } from "next/navigation";
+
 export default function Page(){
+    const customerData=null;
+    if(!customerData) redirect("/dashboard/profile");
     return <>
     <h2>Welcome to customers Page</h2>
     </>
---
+++ src/app/dashboard/profile/page.tsx
@@ -0,0 +1,5 @@
+export default function Page(){
+    return <>
+    <h3>Welcome to Profile Page</h3>
+    </>
+}
