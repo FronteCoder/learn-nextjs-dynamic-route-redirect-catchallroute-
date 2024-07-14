b66661fcca08c24f0c7750526c7ab9d97ddf780b
shivampython <shivammishra.python@gmail.com>
2024-07-14 22:16:37 +0530

    learning how to usePathname and useSearchParams in client components

--- src/app/dashboard/customers/page.tsx
+++ src/app/dashboard/customers/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation";
 
 export default function Page(){
     const customerData=null;
-    if(!customerData) redirect("/dashboard/profile");
+    if(!customerData) redirect("/dashboard/profile?search=shivam&randomvalue=abcde");
     return <>
     <h2>Welcome to customers Page</h2>
     </>
--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,4 +1,12 @@
+"use client"
+import { usePathname, useSearchParams } from "next/navigation"
+
 export default function Page(){
+    const pathname=usePathname();
+    const searchParams=useSearchParams();
+    console.log(pathname,searchParams);
+    console.log(searchParams.get('search'))
+    console.log(searchParams.get('randomvalue'))
     return <>
     <h3>Welcome to Profile Page</h3>
     </>
